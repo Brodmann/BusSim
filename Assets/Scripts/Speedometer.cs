8ca94365f8339d755d1335d7415fec23cafc3094
Brodmann <72983344+Brodmann@users.noreply.github.com>
2022-10-18 14:58:34 +0200

    Speedometer
    Speedometer

---
+++ Assets/Scripts/Speedometer.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+using TMPro;
+
+public class Speedometer : MonoBehaviour
+{
+    [SerializeField] private Rigidbody bus;
+    [SerializeField] private TextMeshProUGUI speedText;
+    private float tmp;
+    void Update()
+    {
+        tmp = Mathf.Sqrt(Mathf.Pow(bus.velocity.sqrMagnitude, 1.3f));
+        speedText.text = tmp.ToString("0") + "km/h";
+    }
+}
